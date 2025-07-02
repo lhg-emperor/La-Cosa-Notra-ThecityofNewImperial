b688e825f8629fa7047b135b5d886d9480aa2758
lhg-emperor <162585551+lhg-emperor@users.noreply.github.com>
2025-07-02 18:44:44 +0700

    Hoàn thiện Navmesh cho Central Island. Thêm nhạc nền cho Intro và Menu

---
+++ Assets/Scenes/Menu/Music.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PersistentMusic : MonoBehaviour
+{
+    private static PersistentMusic instance;
+
+    private void Awake()
+    {
+        // Nếu chưa có instance => giữ lại
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // Không bị hủy khi load scene mới
+        }
+        else
+        {
+            Destroy(gameObject); // Đã có rồi thì xóa cái mới để tránh trùng nhạc
+        }
+    }
+}
